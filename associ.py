028aaa81aaa923d0c72fc622ff865d34889d8527
Victor Lopes <125214784+VictorLPSRD@users.noreply.github.com>
2023-07-21 02:01:47 -0300

    Create associ.py

---
+++ associ.py
@@ -0,0 +1,34 @@
+class Escritor:
+    def __init__(self, nome) -> None:
+        self.nome = nome
+        self._ferramenta = None
+
+    @property
+    def ferramenta(self):
+        return self._ferramenta
+
+    @ferramenta.setter
+    def ferramenta(self, ferramenta):
+        self._ferramenta = ferramenta
+
+
+class FerramentaDeEscrever:
+    def __init__(self, nome):
+        self.nome = nome
+
+    def escrever(self):
+        return f'{self.nome} está escrevendo'
+
+
+escritor = Escritor('Luiz')
+caneta = FerramentaDeEscrever('Caneta Bic')
+lapis= FerramentaDeEscrever('lapiz')
+maquina_de_escrever = FerramentaDeEscrever('Máquina')
+escritor.ferramenta = maquina_de_escrever
+escritor.ferramenta= caneta
+escritor.ferramenta= lapis
+
+print(caneta.escrever())
+
+print(maquina_de_escrever.escrever())
+print(escritor.ferramenta.escrever())
