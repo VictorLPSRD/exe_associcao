52af85a3e425a71f79c91a9d87e8b78ca13d076a
Victor Lopes <125214784+VictorLPSRD@users.noreply.github.com>
2023-07-21 22:06:14 -0300

    Update associ.py

--- associ.py
+++ associ.py
@@ -32,3 +32,52 @@ print(caneta.escrever())
 
 print(maquina_de_escrever.escrever())
 print(escritor.ferramenta.escrever())
+
+#EXECICIO AULA 218 
+
+class Carro:
+    def __init__(self,nome):
+        self.nome = nome
+        self._motor = None
+        self._fabrica = None
+
+    @property
+    def motor(self):
+        return self._motor
+    
+    @motor.setter
+    def motor(self,motor):
+        self._motor= motor
+
+    @property
+    def fabrica(self):
+        return self._fabrica
+    
+    @fabrica.setter
+    def fabrica(self,fabrica):
+        self._fabrica= fabrica
+
+
+class Motor:
+    def __init__(self,nome):
+        self.nome= nome
+
+
+class Fabricante:
+    def __init__(self,nome):
+        self.nome= nome
+
+Carro1=Carro('hb20')
+moto1=Motor('M1')
+fabri=Fabricante('Honda')
+Carro1.motor =moto1
+Carro1.fabrica= fabri
+
+Carro2=Carro('Corrola')
+Carro2.motor =moto1
+Carro2.fabrica= fabri
+
+
+print('Nome= ',Carro1.nome, " MOTOR= ", Carro1.motor.nome,' FABRICANTE=',Carro1.fabrica.nome)
+print()
+print('Nome= ',Carro2.nome, " MOTOR= ", Carro2.motor.nome,' FABRICANTE=',Carro2.fabrica.nome)
