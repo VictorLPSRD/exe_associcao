f86d5f14a24c127d85e448578ec447c2a7cb85f8
Victor Lopes <125214784+VictorLPSRD@users.noreply.github.com>
2023-07-21 22:08:22 -0300

    Update associ.py

--- associ.py
+++ associ.py
@@ -34,6 +34,14 @@ print(maquina_de_escrever.escrever())
 print(escritor.ferramenta.escrever())
 
 #EXECICIO AULA 218 
+# 1 - Crie uma classe Carro (Nome)
+# 2 - Crie uma classe Motor (Nome)
+# 3 - Crie uma classe Fabricante (Nome)
+# 4 - Faça a ligação entre Carro tem um Motor
+# Obs.: Um motor pode ser de vários carros
+# 5 - Faça a ligação entre Carro e um Fabricante
+# Obs.: Um fabricante pode fabricar vários carros
+# Exiba o nome do carro, motor e fabricante na tela
 
 class Carro:
     def __init__(self,nome):
